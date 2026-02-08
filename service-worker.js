64741d32bad4f8d1cf64c38f040dbea35a5770bb
Dave0428 <138314329+Dave0428@users.noreply.github.com>
2026-02-08 23:52:37 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,32 @@
-self.addEventListener("install", e => {
+const CACHE = "ml-invite-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
